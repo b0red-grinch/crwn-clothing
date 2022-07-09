ed72d0b74e5b8f58e8f07a0a41c2dc9009134eec
unknown <raphesezobor@gmail.com>
2022-07-09 10:03:44 -0700

    integrate Stripe payment button

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -13,8 +13,7 @@ export const selectCollections = createSelector(
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
     collections => Object.keys(collections).map(key => collections[key])
-
-)
+);
 
 export const selectCollection =  memoize((collectionUrlParam)  => createSelector(
     [selectCollections],
